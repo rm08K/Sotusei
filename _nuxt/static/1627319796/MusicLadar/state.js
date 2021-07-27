9cb1baddbc199f8e8fd51c1647d7982191056888
rm08K <rm08K@users.noreply.github.com>
2021-07-27 15:02:02 +0000

    deploy: f1b4e97aebecaa9593997ce001d57dbbe2be5373

--- _nuxt/static/1627319796/MusicLadar/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSotusei\u002F_nuxt\u002Fstatic\u002F1627319796",layout:"default",error:a,serverRendered:true,routePath:"\u002FMusicLadar",config:{_app:{basePath:"\u002FSotusei\u002F",assetsPath:"\u002FSotusei\u002F_nuxt\u002F",cdnURL:a}}}}(null));
