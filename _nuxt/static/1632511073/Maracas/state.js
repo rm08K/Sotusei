0f50691c655759efb48fff926da9f77ea8667ca3
rm08K <rm08K@users.noreply.github.com>
2021-09-24 19:17:55 +0000

    deploy: 75199cff27f0e5f1c070abde1bc8a3a62c93d1c0

---
+++ _nuxt/static/1632511073/Maracas/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSotusei\u002F_nuxt\u002Fstatic\u002F1632511073",layout:"default",error:a,serverRendered:true,routePath:"\u002FMaracas",config:{_app:{basePath:"\u002FSotusei\u002F",assetsPath:"\u002FSotusei\u002F_nuxt\u002F",cdnURL:a}}}}(null));
