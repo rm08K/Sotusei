5fda383150784b848aa3a274592292ed021b5479
rm08K <a18dc175@dhw.ac.jp>
2021-10-21 19:29:42 +0900

    ⚙最初にベースを流すためのボタンとして調整

--- pages/Rhythm.vue
+++ pages/Rhythm.vue
@@ -4,7 +4,7 @@
     <button id="btn2" class="btn">Tap!</button>
     <button id="btn3" class="btn">Tap!</button>
     <button id="btn4" class="btn">Tap!</button>
-    <button id="btn5" class="btn">Tap!</button>
+    <button id="btn5" class="mainbtn">Tap!</button>
     <button id="btn6" class="btn">Tap!</button>
     <button id="btn7" class="btn">Tap!</button>
     <button id="btn8" class="btn">Tap!</button>
@@ -13,19 +13,25 @@
 </template>
 
 <style lang="scss">
-  body {
-    margin: 0;
-  }
+body {
+  margin: 0;
+  background-color: #222;
+}
 
-  .container {
-    display: grid;
-    grid-template-rows: 33.3% 33.3% 1fr;
-    grid-template-columns: 33.3% 33.3% 1fr;
-  }
+.container {
+  display: grid;
+  grid-template-rows: 33.3% 33.3% 1fr;
+  grid-template-columns: 33.3% 33.3% 1fr;
+}
 
-  .btn{
-    height: calc(100vh / 3);
-  }
+.btn, .mainbtn {
+  height: calc(100vh / 3);
+  border-radius: 0;
+}
+
+.hidden {
+  visibility: hidden;
+}
 </style>
 
 <script>
@@ -36,25 +42,36 @@ export default {
   methods: {
     rhythm() {
       if (process.client) {
-        console.log("ver1.0")
-        const snd1 = new Audio("rhythm/魔王魂  ドラム2-タム2.mp3")
-        const snd2 = new Audio("rhythm/魔王魂  ピアノ1-Fコード.mp3")
-        const snd5 = new Audio("rhythm/059_dr_bpm140_4-4_rock.mp3")
-        document.getElementById("btn1").addEventListener("click",() => {
+        console.log('ver1.0')
+        let flg = false
+        const snd1 = new Audio('rhythm/魔王魂  ドラム2-タム2.mp3')
+        const snd2 = new Audio('rhythm/魔王魂  ピアノ1-Fコード.mp3')
+        const snd5 = new Audio('rhythm/059_dr_bpm140_4-4_rock.mp3')
+        let elements = document.getElementsByClassName("btn")
+        Array.prototype.forEach.call(elements, function (element) {
+            element.classList.add("hidden")
+        })
+        document.getElementById('btn5').addEventListener('click', () => {
+          if (flg == false) {
+            snd5.currentTime = 0
+            snd5.play()
+            snd5.loop = true
+            flg = true
+            console.log(flg)
+            Array.prototype.forEach.call(elements, function (element) {
+            element.classList.remove("hidden")
+            })
+          }
+        })
+        document.getElementById('btn1').addEventListener('click', () => {
           snd1.currentTime = 0
           snd1.play()
-          console.log("pushed")
+          console.log('pushed')
         })
-        document.getElementById("btn2").addEventListener("click",() => {
+        document.getElementById('btn2').addEventListener('click', () => {
           snd2.currentTime = 0
           snd2.play()
-          console.log("pushed")
-        })
-        document.getElementById("btn5").addEventListener("click",() => {
-          snd5.currentTime = 0
-          snd5.play()
-          snd5.loop = true
-          console.log("pushed")
+          console.log('pushed')
         })
       }
     },
