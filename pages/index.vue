f1035720927436d06844bf773cf9ab0ab8bef341
rm08K <a18dc175@dhw.ac.jp>
2021-12-11 10:44:46 +0900

    ⚙トップページを少し調整

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,15 @@
 <template>
 <div class="topContainer">
+  <h1 class="topTitle">奏でる空想楽器</h1>
   <div class="topText">welcome!</div>
+  <h2 class="topCategory">PC</h2>
   <nuxt-link class="topLink" to="/MusicLadar">MusicLadar</nuxt-link>
-  <nuxt-link class="topLink" to="/Maracas">Maracas</nuxt-link>
   <nuxt-link class="topLink" to="/Rhythm">Rhythm</nuxt-link>
+  <h2 class="topCategory">SmartPhone</h2>
+  <nuxt-link class="topLink" to="/Maracas">Maracas</nuxt-link>
   <nuxt-link class="topLink" to="/SmartCoil">SmartCoil</nuxt-link>
+  <h3 class="topText">奏でる空想楽器とは？</h3>
+  <p class="topText">Web上で音楽を奏でられます。</p>
 </div>
 </template>
 
@@ -18,11 +23,18 @@
   .topContainer {
     margin: auto;
   }
+  .topTitle {
+    text-align: center;
+  }
+  .topCategory {
+    text-align: center;
+  }
   .topText {
     text-align: center;
   }
   .topLink {
     width: 50%;
+    max-width: 300px;
     text-align: center;
     margin: auto;
     margin-bottom: 10px;
