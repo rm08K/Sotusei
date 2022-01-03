288b31cad46ac459fefb6bf3447319c0218d2c52
rm08K <rm08K@users.noreply.github.com>
2022-01-03 12:36:04 +0000

    deploy: 6931dbcd51be607bdf962a521f4e38914eed0dbf

--- _nuxt/static/1641211875/Rhythm/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSotusei\u002F_nuxt\u002Fstatic\u002F1641211875",layout:"default",error:a,serverRendered:true,routePath:"\u002FRhythm",config:{_app:{basePath:"\u002FSotusei\u002F",assetsPath:"\u002FSotusei\u002F_nuxt\u002F",cdnURL:a}}}}(null));
