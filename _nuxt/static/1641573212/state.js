2be5315272ba9d586feb3ba3a2cb33e98654dc76
rm08K <rm08K@users.noreply.github.com>
2022-01-07 16:40:30 +0000

    deploy: 8c8e619e2ee530aea460817924d0e44940ec58ea

--- _nuxt/static/1641573212/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FSotusei\u002F_nuxt\u002Fstatic\u002F1641573212",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FSotusei\u002F",assetsPath:"\u002FSotusei\u002F_nuxt\u002F",cdnURL:a}}}}(null));
