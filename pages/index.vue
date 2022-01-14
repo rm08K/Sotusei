9bb778ec7d30102f20a6179dc64d68dd6f53f4e0
rm08K <a18dc175@dhw.ac.jp>
2022-01-14 16:44:52 +0900

    ⚙スマートフォン用のロゴとページを作成

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,9 @@
   <div class="topContainer">
     <Logo class="test"/>
     <!-- <h1 class="topTitle">奏でる空想楽器</h1> -->
-    <div class="topText">welcome!</div>
-    <h2 class="topCategory-h2">PC</h2>
-    <div class="topCategory">
-      <div class="topCategory-flexBox">
+    <h2 class="topCategory-h2 pc">PC</h2>
+    <div class="topCategory pc">
+      <div class="topCategory-flexBox pc">
         <nuxt-link class="topLink" to="/MusicLadar"><MusicLaderLogo /></nuxt-link>
         <div class="topCategory-flexBox_contents">
           <h3>MusicLader</h3>
@@ -13,7 +12,7 @@
           <p class="topCategory-flexBox_context">マウスでぐりぐりできるオシレーターです。音楽を奏でよう。</p>
         </div>
       </div>
-      <div class="topCategory-flexBox reverse">
+      <div class="topCategory-flexBox pc reverse">
         <div class="topCategory-flexBox_contents">
           <h3>Rhythm</h3>
           <p class="topCategory-flexBox_context">叩くビート ランチパッド</p>
@@ -25,6 +24,24 @@
     <h2 class="topCategory-h2">SmartPhone</h2>
     <div class="topCategory">
       <div class="topCategory-flexBox">
+        <nuxt-link class="topLink" to="/Maracas"><MaracasLogo /></nuxt-link>
+        <div class="topCategory-flexBox_contents">
+          <h3>Maracas</h3>
+          <p class="topCategory-flexBox_context">振って。</p>
+          <p class="topCategory-flexBox_context">なんとスマホがマラカスになっちゃうんです。さぁ振ってみよう！</p>
+        </div>
+      </div>
+      <div class="topCategory-flexBox reverse">
+        <div class="topCategory-flexBox_contents">
+          <h3>SmartCoil</h3>
+          <p class="topCategory-flexBox_context">不思議なコイル</p>
+          <p class="topCategory-flexBox_context">スマートフォンから不思議なサウンド。あなたも音の魔術師になれるかも！</p>
+        </div>
+        <nuxt-link class="topLink" to="/SmartCoil"><SmartCoilLogo /></nuxt-link>
+      </div>
+    </div>
+    <!-- <div class="topCategory">
+      <div class="topCategory-flexBox">
         <nuxt-link class="atopLink smartphone" to="/Maracas">Maracas</nuxt-link>
         <MaracasQR />
       </div>
@@ -32,19 +49,28 @@
         <nuxt-link class="atopLink smartphone" to="/SmartCoil">SmartCoil</nuxt-link>
         <SmartCoilQR />
       </div>
-    </div>
+    </div> -->
     <h3 class="topText">奏でる空想楽器とは？</h3>
     <p class="topText">Web上で音楽を奏でられます。</p>
   </div>
 </template>
 
 <style lang="scss">
+@keyframes appear {
+    0% {
+        opacity: 0;
+    }
+}
+
 body {
   margin: 0;
   background-color: rgb(209, 240, 252);
 }
 a {
   display: block;
+}
+svg {
+  animation: 1.5s appear;
 }
 .topContainer {
   margin: auto;
@@ -57,8 +83,9 @@ a {
     text-align: center;
   }
   &-flexBox {
-    margin-top: 50px;
+    margin-bottom: 80px;
     display: flex;
+    gap: 20px;
     flex-wrap: wrap;
     justify-content: center;
     &_contents {
@@ -93,5 +120,11 @@ a {
   border-radius: 10px;
   color: white;
   background-color: #333;
+}
+
+@media screen and (max-width: 600px) {
+  .pc {
+    display: none;
+  }
 }
 </style>
