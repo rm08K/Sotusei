f6ab573798edea77db956cc39b79535b698e0df7
rm08K <rm08K@users.noreply.github.com>
2022-01-24 06:41:28 +0000

    deploy: 3c94b9abd4b3c0a3aef576e6c6477034ecb537fe

---
+++ _nuxt/static/1643006487/MusicRadar/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/MusicRadar", {data:[{}],fetch:{},mutations:void 0});
---
+++ _nuxt/static/1643006487/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002FMaracas","\u002FMusicRadar","\u002FRhythm","\u002FSmartCoil"]})
